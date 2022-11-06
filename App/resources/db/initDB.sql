d01ac38e12cbf0d26e09b35e7045bd469035c3c9
Renat Skosarev <skosarrrev@gmail.com>
2022-11-06 17:25:36 +0500

    Add init DB script

--- App/resources/db/initDB.sql
+++ App/resources/db/initDB.sql
@@ -3,25 +3,18 @@ DROP TABLE IF EXISTS user;
 
 CREATE TABLE audiofile
 (
-    id        INTEGER NOT NULL
-        PRIMARY KEY AUTOINCREMENT,
-    user_id   INTEGER NOT NULL
-        REFERENCES user,
-    title     TEXT    NOT NULL,
-    author    TEXT    NOT NULL,
-    file_path TEXT    NOT NULL,
-    UNIQUE (
-            user_id,
-            file_path
-        )
+    id        INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id   INTEGER REFERENCES user           NOT NULL,
+    title     TEXT                              NOT NULL,
+    author    TEXT                              NOT NULL,
+    file_path TEXT                              NOT NULL,
+    UNIQUE (user_id, file_path)
 );
 
 CREATE TABLE user
 (
-    id       INTEGER PRIMARY KEY AUTOINCREMENT
-                  NOT NULL,
-    name     TEXT NOT NULL,
-    login    TEXT UNIQUE
-                  NOT NULL,
-    password TEXT NOT NULL
+    id       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name     TEXT                              NOT NULL,
+    login    TEXT UNIQUE                       NOT NULL,
+    password TEXT                              NOT NULL
 );
